8b7ac24145406ac65464a8c4a51f06350de79fca
rtr-dnd <rtr-dnd@users.noreply.github.com>
2022-06-28 13:21:20 +0000

    deploy: 717e56914581e93e757b69532f7496cfc88078eb

---
+++ _next/static/GYScnM4fGtPD9mDf4B3tM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/286-0eb985528bf7e564.js","static/css/9884b83ae568bed3.css","static/chunks/pages/index-0cfb673738f7f6c8.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
